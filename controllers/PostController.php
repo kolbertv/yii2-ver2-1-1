050a48126578bf0c46780ceb19daacf930360122
kolbert vladimir <kolbert@yandex.ru>
2018-10-05 16:49:50 +0300

    лекция 7 минута 9

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -14,12 +14,16 @@ use Yii;
 class PostController extends AppController
 {
 
+    public $layout = 'basic';
+
     public function actionIndex() {
 
         return $this->render('test');
     }
 
     public function actionShow() {
+
+//        $this->layout='basic';
 
         return $this->render('show');
     }
--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,15 +1,39 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-    <h1>Hello, Basic layout</h1>
+use app\assets\AppAsset;
+use yii\helpers\Html;
 
-</body>
-</html>
+
+AppAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+
+    <div class="wrap">
+        <div class="container">
+            <ul class="nav nav-pills">
+                <li role="presentation" class="active"><?= Html::a('Главная', '/')?></li>
+                <li role="presentation"><?= Html::a('Статьи', ['post/index'])?></li>
+                <li role="presentation"><?= Html::a('Статья', ['post/show'])?></li>
+            </ul>
+
+            <?= $content ?>
+
+        </div>
+    </div>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
