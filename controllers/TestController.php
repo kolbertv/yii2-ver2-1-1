d412e226da7dcf162d61a9ea4aa985028243b827
kolbert <kolbert@yandex.ru>
2018-10-08 20:59:20 +0300

    3 и 4 задание 4го урока
    Экшены insert и select

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 
 use app\components\TestService;
+use yii\db\Query;
 use yii\helpers\VarDumper;
 use yii\web\Controller;
 use app\models\Test;
@@ -38,6 +39,40 @@ class TestController extends Controller
             'model' => $model,
         ]);
     }
+
+    public function actionInsert()
+    {
+        $result = \Yii::$app->db->createCommand()->insert('user', array(
+                'username'=> 'vasya',
+                'name'=> 'vastya',
+                'password_hash'=> '1234567890',
+                'access_token'=> '123456789',
+                'auth_key'=> '1234567',
+                'creator_id'=> '234567',
+                'updater_id'=> '1234567',
+                'created_at'=> '23456',
+                'updated_at'=> '234567',
+
+        ))->execute();
+
+        return 'данные добавлены';
+
+    }
+
+
+    public function actionSelect()
+    {
+
+        $query1 = (new Query())->from('user')->where('id = 1');
+        $query2 = (new Query())->from('user')->where('id > 1')->orderBy('name ASC');
+        $result1 = $query1->all();
+        $result2 = $query2->all();
+
+            return VarDumper::dumpAsString($result2, 5, true);
+
+    }
+
+
 
 
 }
