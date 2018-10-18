4e6167d8f9a26e2b0272d050ea7e05ec3c9e6acc
kolbert <kolbert@yandex.ru>
2018-10-19 00:20:05 +0300

    отключил требование быть авторизованным

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -23,15 +23,15 @@ class UserController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
+//            'access' => [
+//                'class' => AccessControl::className(),
+//                'rules' => [
+//                    [
+//                        'allow' => true,
+//                        'roles' => ['@'],
+//                    ],
+//                ],
+//            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
